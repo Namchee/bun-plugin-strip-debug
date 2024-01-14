306cff04a8f384a0d09d99a9bc6eef3b57e365cd
Cristopher Namchee <cristophernamchee12@gmail.com>
2024-01-14 21:25:10 +0700

    chore: experimenting with debugger statements

--- main.ts
+++ main.ts
@@ -1,8 +1,11 @@
 import ts from 'typescript';
 
-const stripPatterns = [/console\.\w?/, /debugger/];
+const stripPatterns = [/console\.\w?/];
 
-const printer = ts.createPrinter({ newLine: ts.NewLineKind.LineFeed });
+const printer = ts.createPrinter({
+  newLine: ts.NewLineKind.LineFeed,
+  omitTrailingSemicolon: true,
+});
 
 function flattenExpression(node: ts.CallExpression): string {
   const tokens: string[] = [];
@@ -25,13 +28,17 @@ const stripTransformer: ts.TransformerFactory<ts.SourceFile> = (
   ctx: ts.TransformationContext
 ) => {
   return (source: ts.SourceFile) => {
-    const walk: ts.Visitor = (node: ts.Node) => {
+    const walk = (node: ts.Node) => {
       if (ts.isCallExpression(node)) {
         const flattened = flattenExpression(node);
 
         return stripPatterns.some((pattern) => pattern.test(flattened))
           ? ctx.factory.createIdentifier('')
           : node;
+      }
+
+      if (ts.isDebuggerStatement(node)) {
+        return ctx.factory.createIdentifier('');
       }
 
       return ts.visitEachChild(node, walk, ctx);
@@ -41,20 +48,25 @@ const stripTransformer: ts.TransformerFactory<ts.SourceFile> = (
   };
 };
 
+function stripOrphanedSemicolons(output: string) {}
+
 (() => {
   const source = ts.createSourceFile(
     '',
     `const a = 'Hello World';
-console.log('aaa')
-console.log('cccccccc')`,
+const b = 'c'; console.log('aaa'); console.table({ foo: 'bar' });
+console.log('cccccccc');`,
     ts.ScriptTarget.ES2015,
     false,
     ts.ScriptKind.TS
   );
 
   const result = ts.transform(source, [stripTransformer]);
-
-  console.log(
-    printer.printNode(ts.EmitHint.Unspecified, result.transformed[0], source)
+  const transformed = printer.printNode(
+    ts.EmitHint.Unspecified,
+    result.transformed[0],
+    source
   );
+
+  console.log(transformed);
 })();
